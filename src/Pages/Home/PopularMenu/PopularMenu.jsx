eea079d404ec281ef963b66dbd6d817aeb3c60a1
Abir <abirmahmud221@gmail.com>
2023-11-14 17:14:43 +0600

    65-3

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,19 +1,23 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/Shared/SectionTitle/SectionTitle";
 import MenuItems from "../../../Components/Shared/MenuItems/MenuItems";
+import useMenu from "../../../Hooks/useMenu";
 
 
 const PopularMenu = () => {
 
-    const [menu, setMenu] = useState([])
-    useEffect(() => {
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const popularItems = data.filter(item => item.category === 'popular');
-            setMenu(popularItems);
-        })
-    }, [])
+    const [menu] = useMenu();
+    const popular = menu.filter(item => item.category === 'popular')
+
+    // const [menu, setMenu] = useState([])
+    // useEffect(() => {
+    //     fetch('menu.json')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         const popularItems = data.filter(item => item.category === 'popular');
+    //         setMenu(popularItems);
+    //     })
+    // }, [])
 
     return (
         <section className="my-12">
@@ -23,7 +27,7 @@ const PopularMenu = () => {
             ></SectionTitle>
             <div className="grid md:grid-cols-2 gap-10">
                 {
-                    menu.map(item => <MenuItems key={item._id} item={item}></MenuItems>)
+                    popular.map(item => <MenuItems key={item._id} item={item}></MenuItems>)
                 }
             </div>
         </section>
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../../Components/Shared/Cover/Cover";
 import menuBg from "../../../assets/menu/banner3.jpg"
-import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+
 
 const Menu = () => {
   return (
@@ -10,13 +10,7 @@ const Menu = () => {
         <title>Bistro Boss | Menu</title>
       </Helmet>
       <Cover img={menuBg} title="Our Menu"></Cover>
-      <PopularMenu></PopularMenu>
-      <Cover img={menuBg} title="Our Menu"></Cover>
-      <PopularMenu></PopularMenu>
-      <Cover img={menuBg} title="Our Menu"></Cover>
-      <PopularMenu></PopularMenu>
-      <Cover img={menuBg} title="Our Menu"></Cover>
-      <PopularMenu></PopularMenu>
+      
     </div>
   );
 };
