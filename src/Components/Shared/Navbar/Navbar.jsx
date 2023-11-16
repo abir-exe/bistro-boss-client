4ac0c462e5898ce093cf0787e736747d08e09a0f
Abir <abirmahmud221@gmail.com>
2023-11-16 20:11:06 +0600

    66-1

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -12,6 +12,9 @@ const Navbar = () => {
       <li>
         <Link to="/order/salad">Order Now</Link>
       </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
       
     </>
   );
