24b9501617849998d1c4a7bf95ffc4fe07d8d929
Abir <abirmahmud221@gmail.com>
2023-11-24 09:10:04 +0600

    70-1

---
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,15 @@
+import SectionTitle from "../../../Components/Shared/SectionTitle/SectionTitle";
+
+
+const Payment = () => {
+    return (
+        <div>
+            <SectionTitle heading="Payment" subHeading="Please pay to eat"></SectionTitle>
+            <div>
+              <h2>Teka tumi uira uira asho</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Payment;
--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -14,6 +14,7 @@ import AdminRoute from "./AdminRoute";
 import AddItems from "../Pages/Dashboard/AddItems/AddItems";
 import ManageItems from "../Pages/Dashboard/ManageItems/ManageItems";
 import UpdateItem from "../Pages/Dashboard/UpdateItem/UpdateItem";
+import Payment from "../Pages/Dashboard/Payment/Payment";
 
 const router = createBrowserRouter([
   {
@@ -62,6 +63,10 @@ const router = createBrowserRouter([
       {
         path: "cart",
         element: <Cart></Cart>,
+      },
+      {
+        path: 'payment',
+        element: <Payment></Payment>
       },
       // admin routes
       {
